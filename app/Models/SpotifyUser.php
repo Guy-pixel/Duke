4d82be9b146a2ac78fe50dd69280c57dff85e2b1
Guy-pixel <guygasson@hotmail.co.uk>
2022-05-10 19:04:07 +0100

    Device list and skip to next

--- app/Models/SpotifyUser.php
+++ app/Models/SpotifyUser.php
@@ -4,17 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use JetBrains\PhpStorm\NoReturn;
 
 class SpotifyUser extends Model
 {
     use HasFactory;
-    public $client_id;
-    public $client_secret;
-    public $authToken;
-    public function construct($client_id = NULL, $client_secret = NULL, $authToken = NULL){
+    public string $client_id;
+    public string $client_secret;
+    public string $accessToken;
+    public function __construct($client_id = '', $client_secret = ''){
         $this->client_id=$client_id;
         $this->client_secret=$client_secret;
-        $this->authToken=$authToken;
     }
     public function getUserToken(string $code, string $devAppID, string $devAppSecret){
         $params=array(
@@ -46,9 +46,53 @@ class SpotifyUser extends Model
         $response=json_decode(curl_exec($curl));
         curl_close($curl);
         if(isset($response->access_token)) {
-            $this->authToken = $response->access_token;
+            $this->accessToken = $response->access_token;
         }
         return $response;
+    }
+    public function getDevices(){
+        $curl=curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.spotify.com/v1/me/player/devices',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer ' . $this->accessToken,
+                'Content-Type: application/json'
+                )
+            )
+        );
+        $response=json_decode(curl_exec($curl));
+        curl_close($curl);
+        return $response;
+    }
+    public function skipToNext(){
+        $curl=curl_init();
+        curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://api.spotify.com/v1/me/player/next',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_HTTPHEADER => array(
+                    'Authorization: Bearer ' . $this->accessToken,
+                    'Content-Type: application/json',
+                    'Content-Length: 0'
+                )
+            )
+        );
+        $response=curl_exec($curl);
+        curl_close($curl);
+        print_r($response);
+
     }
 
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,13 @@ use App\Models\SpotifyUser;
 
 <?php
     $devApp = new SpotifyDev('3dbd8531a08c4b26920d00ac58ec5adb', '292ad2b5bcb2443d970fa3cf945e0c1e');
-    print('<strong>client id:</strong>' . base64_encode($devApp->client_id) . '<br/><strong>client secret:</strong>' . base64_encode($devApp->client_secret) . '<br/>');
-    echo '<strong> Dev Token: </strong>' . $devApp->getToken() . '<br/>';
+    print('<strong>client id: </strong>' . base64_encode($devApp->client_id) . '<br/><strong>client secret:</strong>' . base64_encode($devApp->client_secret) . '<br/>');
+    echo '<strong>Dev Token: </strong>' . $devApp->getToken() . '<br/>';
+
     if(isset($_GET['code'])){
-        echo '<strong>User Code:</strong>' . $_GET['code'] . '<br/>';
+        echo '<strong>User Code: </strong>' . $_GET['code'] . '<br/>';
         $currentUser = new SpotifyUser();
+        echo '<strong>User Object: </strong>';
         print_r($currentUser->getUserToken($_GET['code'], $devApp->client_id, $devApp->client_secret));
     }
     ?>
@@ -19,8 +21,26 @@ use App\Models\SpotifyUser;
     <br/>
 
     <div>
-        <a href='https://accounts.spotify.com/authorize?<?php echo($devApp->createAuthorizationLink()) ?>'>
+        <a href='https://accounts.spotify.com/authorize?<?= ($devApp->createAuthorizationLink()) ?>'>
             Log Into Spotify
         </a>
     </div>
+    <?php
+    if(isset($currentUser->accessToken)){
+        $deviceList = $currentUser->getDevices()->devices;
+        print_r($deviceList);
+        echo '<br/>';
+        foreach($deviceList as $key=>$device){
+           if($device->is_active){
+               $activeDevice=[
+                   'id'    =>  $device->id,
+                   'name'  =>  $device->name,
+               ];
+           }
+        }
+        $currentUser->skipToNext();
+    }
+
+    ?>
+
 </x-layout>
