2de83993ca693cd91175e8f2f6ffbea5b8325f27
Guy-pixel <guygasson@hotmail.co.uk>
2022-08-20 16:03:00 +0100

    Update SpotifySongController.php

--- app/Http/Controllers/SpotifySongController.php
+++ app/Http/Controllers/SpotifySongController.php
@@ -20,23 +20,37 @@ class SpotifySongController extends Controller
     public function nextSong()
     {
         session_start();
+<<<<<<< Updated upstream
         $currentUser = new SpotifyUser($_SESSION['access_token'], $_SESSION['refresh_token']);
         $currentUser->next();
+=======
+        $currentUser = new SpotifyUser($_SESSION['access_token'], '');
+        $currentUser->skipToNext();
+>>>>>>> Stashed changes
         session_abort();
     }
 
     public function previousSong()
     {
         session_start();
+<<<<<<< Updated upstream
         $currentUser = new SpotifyUser($_SESSION['access_token'], $_SESSION['refresh_token']);
         $currentUser->previous();
+=======
+        $currentUser = new SpotifyUser($_SESSION['access_token'], '');
+        $currentUser->skipToPrevious();
+>>>>>>> Stashed changes
         session_abort();
     }
 
     public function resumeSong()
     {
         session_start();
+<<<<<<< Updated upstream
         $currentUser = new SpotifyUser($_SESSION['access_token'], $_SESSION['refresh_token']);
+=======
+        $currentUser = new SpotifyUser($_SESSION['access_token'], '');
+>>>>>>> Stashed changes
         $currentUser->resume();
         session_abort();
     }
@@ -44,7 +58,11 @@ class SpotifySongController extends Controller
     public function pauseSong()
     {
         session_start();
+<<<<<<< Updated upstream
         $currentUser = new SpotifyUser($_SESSION['access_token'], $_SESSION['refresh_token']);
+=======
+        $currentUser = new SpotifyUser($_SESSION['access_token'], '');
+>>>>>>> Stashed changes
         $currentUser->pause();
         session_abort();
     }
